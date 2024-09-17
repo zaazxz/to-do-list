93f33041e1925d30be57da0bfd016a9041d07ae5
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-09-17 09:42:11 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='bg-red-500'>Button tailwind</button>
+    </>
+  )
+}
+
+export default App
